03afd851bb7c0fc1d332a21ca21f21eaf7b932dc
m4nuelcc <manuelcarabiac@gmail.com>
2021-12-21 11:16:54 +0100

    Utilizacion del la anotacion @InitBinder con el metodo miBinder que quita los espacios en blanco del campo

--- src/main/java/es/manu/spring/mvc/AlumnoController.java
+++ src/main/java/es/manu/spring/mvc/AlumnoController.java
@@ -2,15 +2,31 @@ package es.manu.spring.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/alumno")
 public class AlumnoController {
+	
+	
+	// el metodo miBinder controla para que los campos String del formulario
+	// no puedan contener espacios y asi envitar datos vacios
+	@InitBinder
+	public void miBinder(WebDataBinder binder) {
+		
+//		StringTrimmerEditor recortaEspaciosBlanco = new StringTrimmerEditor(true);
+//		binder.registerCustomEditor(String.class, recortaEspaciosBlanco);
+		
+		binder.registerCustomEditor(String.class, new StringTrimmerEditor(false));
+		
+	}
 
 	@RequestMapping("/muestraFormulario")
 	public String muestraFormulario(Model modelo) {
